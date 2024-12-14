0b296b24378b15fb4d5543b4a2683f5e255cf129
Y. S <25735979+YS-jack@users.noreply.github.com>
2024-12-14 10:36:02 +0900

    translates widget while checking recursively, instead of fetching all valid widgets then translating
    because creating sql query is easier when ran from root to leaf

--- src/main/java/com/RuneLingual/Wigets/WidgetCapture.java
+++ src/main/java/com/RuneLingual/Wigets/WidgetCapture.java
@@ -6,6 +6,7 @@ import com.RuneLingual.RuneLingualPlugin;
 import com.RuneLingual.commonFunctions.Colors;
 import com.RuneLingual.commonFunctions.Transformer;
 import lombok.Getter;
+import net.runelite.api.Client;
 import net.runelite.api.widgets.ComponentID;
 import net.runelite.api.widgets.InterfaceID;
 import net.runelite.api.widgets.Widget;
@@ -22,6 +23,8 @@ public class WidgetCapture {
     private WidgetsUtilRLingual widgetsUtilRLingual;
     @Inject
     private DialogTranslator dialogTranslator;
+    @Inject
+    Client client;
 
     @Inject
     public WidgetCapture(RuneLingualPlugin plugin){
@@ -29,28 +32,58 @@ public class WidgetCapture {
     }
 
     public void translateWidget(){
-        Widget[] validWidgets = widgetsUtilRLingual.getAllChildren();
-        for(Widget validWidget : validWidgets){
-            int widgetGroup = WidgetUtil.componentToInterface(validWidget.getId());
-            if(widgetGroup == InterfaceID.CHATBOX){// skip all chatbox widgets for now TODO: chatbox buttons should be translated
-                continue;
-            }
-            if(widgetGroup == InterfaceID.DIALOG_NPC
-                || widgetGroup == InterfaceID.DIALOG_PLAYER
-                || widgetGroup == InterfaceID.DIALOG_OPTION){
-                dialogTranslator.handleDialogs(validWidget);
-            } else if(!validWidget.getText().isEmpty()
-                    && !validWidget.getText().contains("<img=") // check if already translated to japanese. TODO: need something else after adding other languages
-                    && plugin.getConfig().getInterfaceTextConfig().equals(RuneLingualConfig.ingameTranslationConfig.USE_API)) {
-                // if its only numbers and symbols dont do anything
-                String re = "^[^\\p{Alpha}]+$";
-                if (validWidget.getText().matches(re))
-                    continue;
-                Colors[] textColor = Colors.getColorArray(validWidget.getText(),Colors.black);
-                // for now only translate interfaces and buttons with API
-                widgetsUtilRLingual.setWidgetText_ApiTranslation(validWidget, validWidget.getText(), textColor[0]);
-
-            }
+        Widget[] roots = client.getWidgetRoots();
+        for (Widget root : roots) {
+            translateWidgetRecursive(root);
         }
     }
+
+    private void translateWidgetRecursive(Widget widget) {
+        int widgetId = widget.getId();
+
+        int[] widgetIdsToIgnore = new int[]{ComponentID.CHATBOX_INPUT};
+        // stop the recursion if the widget is hidden or should be ignored
+        if (widget.isHidden() || arrayContainsInt(widgetIdsToIgnore, widgetId)) {
+            return;
+        }
+
+        for (Widget dynamicChild : widget.getDynamicChildren()) {translateWidgetRecursive(dynamicChild);}
+        for (Widget nestedChild : widget.getNestedChildren()) {translateWidgetRecursive(nestedChild);}
+        for (Widget staticChild : widget.getStaticChildren()) {translateWidgetRecursive(staticChild);}
+
+        int widgetGroup = WidgetUtil.componentToInterface(widget.getId());
+        if (widgetGroup == InterfaceID.CHATBOX) {// skip all chatbox widgets for now TODO: chatbox buttons should be translated
+            return;
+        }
+        if (widgetGroup == InterfaceID.DIALOG_NPC
+                || widgetGroup == InterfaceID.DIALOG_PLAYER
+                || widgetGroup == InterfaceID.DIALOG_OPTION) {
+            dialogTranslator.handleDialogs(widget);
+        } else if (!widget.getText().isEmpty()
+                && !widget.getText().contains("<img=") // check if already translated to japanese. TODO: need something else after adding other languages
+                && plugin.getConfig().getInterfaceTextConfig().equals(RuneLingualConfig.ingameTranslationConfig.USE_API)) {
+
+            // if its only numbers and symbols dont do anything
+            String re = "^[^\\p{Alpha}]+$";
+            if (widget.getText().matches(re))
+                return;
+
+            Colors[] textColor = Colors.getColorArray(widget.getText(), Colors.black);
+            // for now only translate interfaces and buttons with API
+            widgetsUtilRLingual.setWidgetText_ApiTranslation(widget, widget.getText(), textColor[0]);
+
+        }
+    }
+
+    private static boolean arrayContainsInt(int[] array, int target) {
+        for (int element : array) {
+            if (element == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
+
