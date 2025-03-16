8cbf713ededbdf57ed5d35f18c58d13f8a22e1b3
Y. S <25735979+YS-jack@users.noreply.github.com>
2025-03-16 14:13:30 +0900

    fixed issue with DB when switching lang in order of JA > EN > JA
    this caused menus to not translate.

--- src/main/java/com/RuneLingual/RuneLingualPlugin.java
+++ src/main/java/com/RuneLingual/RuneLingualPlugin.java
@@ -265,7 +265,12 @@ public class RuneLingualPlugin extends Plugin {
         // if language is changed
         if (targetLanguage != config.getSelectedLanguage()) {
             targetLanguage = config.getSelectedLanguage();
-            h2Manager.closeConn();
+
+            if (targetLanguage != LangCodeSelectableList.ENGLISH) {
+                //close current connection
+                h2Manager.closeConn();
+            }
+
             if (targetLanguage == LangCodeSelectableList.ENGLISH) {
                 clientToolBar.removeNavigation(navButton);
                 return;
